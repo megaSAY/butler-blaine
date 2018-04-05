02c97809ac012182951dbb45b94786ddbcf5a884
megasay <alex.skarkov@gmail.com>
2018-04-05 10:23:39 +0300

    Добавлен вызов обновления базы данных UpdateDB

--- accountant/api.go
+++ accountant/api.go
@@ -1,10 +1,21 @@
 package accountant
 
-//GetBalance  In (int): Account number. Out(*float32): Balance
+//UpdateDB In: nil, Out: nil
+const UpdateDB = "Accountant.UpdateDB"
+
+//GetBalance In (int): Account number. Out(*float32): Balance
 const GetBalance = "Accountant.GetBalance"
 
-//GetAccountsQuantity  In (int): nil. Out(*int): Accounts quantity
+/*
+//GetAccountsQuantity In (int): nil. Out(*int): Accounts quantity
 const GetAccountsQuantity = "Accountant.GetAccountsQuantity"
+
+//GetAccountNum In (int): Account index. Out(*int): Account name
+const GetAccountNum = "Accountant.GetAccountsNum"
 
 //GetAccountName In (int): Account index. Out(*int): Account name
 const GetAccountName = "Accountant.GetAccountsName"
+
+//SetAccountName In (int): Account index. Out(*int): Account name
+const SetAccountName = "Accountant.SetAccountsName"
+*/
