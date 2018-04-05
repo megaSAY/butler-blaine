63b43cd853d90a1131f345fe5d4e7baf2442834c
megasay <alex.skarkov@gmail.com>
2018-04-05 22:15:49 +0300

    Пример

--- cmd/accountant/examples/accclient.go
+++ cmd/accountant/examples/accclient.go
@@ -22,10 +22,37 @@ func main() {
 	}
 
 	var reply float32
+
+	/*
+		fmt.Println(accountant.UpdateDB)
+		err = client.Call(accountant.UpdateDB, to, &reply)
+		if err != nil {
+			log.Fatal("Account error:", err)
+		}
+		fmt.Println("reply: ", reply)
+	*/
+
+	fmt.Println(accountant.GetBalance)
 	err = client.Call(accountant.GetBalance, 8336, &reply)
 	if err != nil {
 		log.Fatal("Account error:", err)
 	}
-
 	fmt.Println("reply: ", reply)
+
+	iReply := 0
+	fmt.Println(accountant.GetAccountsQuantity)
+	err = client.Call(accountant.GetAccountsQuantity, 0, &iReply)
+	if err != nil {
+		log.Fatal("Account error:", err)
+	}
+	fmt.Println("reply: ", iReply)
+
+	balances := []accountant.Balance{}
+	fmt.Println(accountant.GetFullBalance)
+	err = client.Call(accountant.GetFullBalance, iReply, &balances)
+	if err != nil {
+		log.Fatal("Account error:", err)
+	}
+	fmt.Println("reply: ", balances)
+
 }
