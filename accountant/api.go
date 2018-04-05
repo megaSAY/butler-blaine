5e6d4151a07c4c7cac8d80298c56c30ee9f5641b
megasay <alex.skarkov@gmail.com>
2018-04-05 22:13:49 +0300

    Добавлены новые команды

--- accountant/api.go
+++ accountant/api.go
@@ -1,21 +1,19 @@
 package accountant
 
-//UpdateDB In: nil, Out: nil
+//UpdateDB Send: 0. Reply: 0
 const UpdateDB = "Accountant.UpdateDB"
 
-//GetBalance In (int): Account number. Out(*float32): Balance
+//GetBalance Send: int Account number. Reply: *float64 Balance
 const GetBalance = "Accountant.GetBalance"
 
-/*
-//GetAccountsQuantity In (int): nil. Out(*int): Accounts quantity
+//GetAccountsQuantity Send: 0. Reply: *int Accounts quantity
 const GetAccountsQuantity = "Accountant.GetAccountsQuantity"
 
-//GetAccountNum In (int): Account index. Out(*int): Account name
-const GetAccountNum = "Accountant.GetAccountsNum"
+//GetFullBalance Send int Accounts quantity. Reply: *[]Balance Accounts balance slice
+const GetFullBalance = "Accountant.GetFullBalance"
 
-//GetAccountName In (int): Account index. Out(*int): Account name
-const GetAccountName = "Accountant.GetAccountsName"
-
-//SetAccountName In (int): Account index. Out(*int): Account name
-const SetAccountName = "Accountant.SetAccountsName"
-*/
+//Balance Account int, Dostupno string
+type Balance struct {
+	Account  int
+	Dostupno string
+}
